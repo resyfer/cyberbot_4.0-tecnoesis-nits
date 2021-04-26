98be7396560ac1ad1e60125560a9e9c5634098e6
resyfer <palsaurav.2020@gmail.com>
2021-04-26 10:28:21 +0530

    completed game mechanics

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # cyberbot_4.0-tecnoesis-nits
+Team Members: Saurav Pal (resyfer)
+
+[Project Website Link](https://resyfer.github.io/cyberbot_4.0-tecnoesis-nits/)
+
 My project for Cyberbot 4.0 for Tecnoesis, National Institute of Technology, Silchar 2021
--- js/index.js
+++ js/index.js
@@ -31,5 +31,75 @@ var body = document.getElementsByTagName("body")[0];
 setInterval(() => {
   var boxDiv = document.createElement("div");
   boxDiv.setAttribute("class", "box");
-  body.appendChild(boxDiv);
-}, 3000)
+  
+  
+  //Random Height for Boulders
+  var viewHeight = window.innerHeight;
+  var randHeight = (Math.random()*3000) % (viewHeight);
+  body.appendChild(boxDiv); //Adding new boulder
+  
+  var boulders = document.getElementsByClassName('box');
+  var lastBoulder = boulders[boulders.length - 1];
+  lastBoulder.style.top = randHeight + "px";
+
+  
+  //Reducing transition time as time increases
+  var scoreMultiplier = ((1000 - score)/1000)*5;
+  lastBoulder.style.animationDuration = scoreMultiplier + 's';
+}, 3000);
+
+//Highest Score
+var highScore = localStorage.getItem('highScore');
+var bestScore = document.getElementById('bestScore');
+if(highScore == null) {
+  bestScore.innerHTML = '0';
+} else {
+  bestScore.innerHTML = highScore;
+}
+
+function setHighScore() { 
+  if(highScore == null) {
+    localStorage.setItem('highScore', score);
+  } else {
+    localStorage.setItem('highScore', Math.max(score, highScore));
+    highScore = localStorage.getItem('highScore');
+  }
+  bestScore.innerHTML = highScore;
+}
+
+//Collision Check
+
+function checkCollision(e1, e2) {
+  if (e1.length && e1.length > 1) {
+    e1 = e1[0];
+  }
+  if (e2.length && e2.length > 1){
+    e2 = e2[0];
+  }
+  const rect1 = e1 instanceof Element ? e1.getBoundingClientRect() : false;
+  const rect2 = e2 instanceof Element ? e2.getBoundingClientRect() : false;
+  
+  console.log(rect1, rect2);
+ 
+  let overlap = false;
+ 
+  if (rect1 && rect2) {
+    overlap = !(
+      rect1.right < rect2.left || 
+      rect1.left > rect2.right || 
+      rect1.bottom < rect2.top || 
+      rect1.top > rect2.bottom
+    );
+  }
+  return overlap;
+};
+setInterval(() => {
+  var boxList = document.getElementsByClassName('box');
+  for(var i = 0; i<boxList.length; i++) {
+    if(checkCollision(boxList[i], rocket) == true) {
+      alert("Your Score: " + score);
+      setHighScore();
+      location.reload();
+    }
+  }  
+}, 200);
